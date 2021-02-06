2c0a2d639b20f4efebe900f011130d8cc5075a38
Filipe-tizz0 <filipetizzo@gmail.com>
2021-02-05 21:40:52 -0300

    arquivos para jogar

---
+++ Player_teste_walk/Assets/player_walk.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_walk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int player_speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            transform.position = transform.position + Camera.main.transform.forward * player_speed * Time.deltaTime;
+        }
+    }
+}
